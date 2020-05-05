d68ee0db5a5b8fc2f3d6512945b2e65968748f7f
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2020-05-05 17:31:40 +0300

    deploy firebase hosting

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ npm install
 npm start   
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## [Demo](https://drag-drop-color.web.app/)
---
+++ dist/precache-manifest.baf65108d3ff3e3116c5ca89c1be5eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616953fc20d27e11b1592916d511cac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c20c083ac2b512d4db9",
+    "url": "/static/css/main.6b89bc67.chunk.css"
+  },
+  {
+    "revision": "8b6cb7f4cd790300ca4f",
+    "url": "/static/js/2.08fb3ac2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08fb3ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c20c083ac2b512d4db9",
+    "url": "/static/js/main.26eb7210.chunk.js"
+  },
+  {
+    "revision": "248dabccf6b9105a3f22",
+    "url": "/static/js/runtime-main.12fd1ffa.js"
+  }
+]);
